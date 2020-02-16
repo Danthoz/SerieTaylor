65aac8dbc80057c9b5ceff0820774e79b60e8c09
Danthoz <>
2020-02-16 01:21:35 -0500

    BugCorregido

---
+++ src/modelo/Modelo.java
@@ -0,0 +1,72 @@
+package modelo;
+
+/**
+ *
+ * @author Daniel Torres 287871
+ */
+public class Modelo {
+ 
+    private int valorN;
+    private double angulo;
+    private double salida;
+
+    public int getValorN() {
+        return valorN;
+    }
+
+    public void setValorN(int valorN) {
+        this.valorN = valorN;
+    }
+
+    public double getAngulo() {
+        return angulo;
+    }
+
+    public void setAngulo(double angulo) {
+        this.angulo = angulo;
+    }
+
+    public double getSalida() {
+        return salida;
+    }
+
+    public void setSalida(double salida) {
+        this.salida = salida;
+    }
+
+    //Metodo que calcula en seno con serie de Taylor
+    public double seno (){
+        //Se divide la operacion en divisor, dividendo, signo y una expresion recurrente
+        double x , dividendo, divisor, signo, exp; 
+        this.salida = 0;
+        //Se pasa el angulo de grados a radianes
+        x = Math.toRadians(this.angulo);
+        //Correccion Pi
+        //Cuando se realiza el calculo con Pi arroja valores errados
+        //Cuando se reemplaza pi por 3.1 se corrije el resultado
+        if(x%Math.PI==0) {
+            x=3.1;
+        }
+        //FOR utilizado para el número de iteraciones
+        for (int i = 0; i <= this.valorN; i++) {
+            exp = 2*i+1;
+            //Calcula dividendo
+            dividendo=Math.pow(x,exp);
+            //Calcula divisor
+            divisor = 1;
+            //FOR utilizado para el factorial
+            for (int j = 1; j <= exp; j++) {
+                divisor = divisor * j;
+            }
+            //Calcula si el signo es positivo o negativo
+            if (i%2==0) {
+                signo =  1;
+            } else {
+                signo = -1;
+            }
+            //Se realiza sumatoria por cada iteracion
+            this.salida = this.salida + (dividendo/divisor)*signo;
+        }        
+        return this.salida;
+    }
+}
---
+++ src/serietaylor/SerieTaylor.java
@@ -0,0 +1,24 @@
+package serietaylor;
+
+import controlador.Controlador;
+import modelo.Modelo;
+import vista.Vista;
+
+/**
+ * @author Daniel Torres 28781
+ */
+public class SerieTaylor {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Se instancian clases y se inicia el controlador
+        Modelo mod = new Modelo();
+        Vista view = new Vista();
+        Controlador ctrl = new Controlador(view,mod);
+        ctrl.iniciar();
+        view.setVisible(true);
+        
+    }
+    
+}
