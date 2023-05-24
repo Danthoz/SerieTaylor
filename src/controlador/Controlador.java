5e6950033b3cca2a07f107df34e26d6fd2ccf7c0
Danthoz <33380531+Danthoz@users.noreply.github.com>
2023-05-23 23:14:00 -0500

    Add files via upload
    Se actualiza proyecto con OpenJDK 1.8.0

---
+++ src/controlador/Controlador.java
@@ -0,0 +1,32 @@
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import modelo.Modelo;
+import vista.Vista;
+/**
+ * @author Daniel Torres 287871
+ */
+public class Controlador implements ActionListener{
+    private Vista view;
+    private Modelo model;
+    //Instancia
+    public Controlador(Vista view,Modelo model){
+        this.view = view;
+        this.model = model;
+        this.view.btnCalcular.addActionListener(this);
+    }
+    //Inicia el Jframe
+    public void iniciar(){
+        view.setTitle("Serie Taylor (Sen)");
+        view.setLocationRelativeTo(null);
+    }
+    //Programación para el evento en el boton
+    public void actionPerformed(ActionEvent e){
+        model.setValorN(Integer.parseInt(view.txtN.getText()));
+        model.setAngulo(Double.parseDouble(view.txtX.getText()));
+        model.seno();
+        view.txtResultado.setText(String.valueOf(model.getSalida()));
+    }
+
+}
